1ef3fc04ccaf6f9dd8c94ce8356959bae81a8666
unknown <1004tinh@gmail.com>
2022-04-08 13:02:29 +0700

    add

--- index.js
+++ index.js
@@ -2,50 +2,58 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const config = require('./config');
-const firebase = require('./db');
+const firebase = require('firebase');
 
 const FCM = require('fcm-node')
 var serverKey = require('./privateKey.json')
 
 const app = express();
+const db = firebase.initializeApp({
+    apiKey: "AIzaSyCFkvTx55DvTZP01VjmlbkhQPUEb86XJTA",
+    authDomain: "realtime-flutter-1b867.firebaseapp.com",
+    databaseURL: "https://realtime-flutter-1b867-default-rtdb.firebaseio.com",
+    projectId: "realtime-flutter-1b867",
+    storageBucket: "realtime-flutter-1b867.appspot.com",
+    messagingSenderId: "794380958759",
+    appId: "1:794380958759:web:1111de432305e1ae9ae202"
+});
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 var fcm = new FCM(serverKey);
 
-var databaseDB = firebase.database();
+var databaseDB = db.database();
 // databaseDB.ref("notification/status").set("yes");
 
 const sendMessage = (token, title, body) => {
-    var message = { 
+    var message = {
         to: token,
         // collapse_key: '...',
-    
+
         notification: {
             title: title,
             body: body
         },
-    
+
         data: {  //you can send only notification or only data(or include both)
             my_key: 'my value',
             my_another_key: 'my another value'
         }
     }
-    
+
     fcm.send(message, function (err, response) {
         if (err) {
             console.log("Something has gone wrong!")
         } else {
 
             console.log("Successfully sent with response: ", response)
-            const add_notification = new Notification({ 
+            const add_notification = new Notification({
                 token: req.body.token,
                 title: req.body.title,
-                content:  req.body.body
+                content: req.body.body
             })
-        
+
         }
     })
 }
@@ -55,23 +63,25 @@ setInterval(() => {
         .then(function (snapshot) {
             if (snapshot.val() == "yes") {
                 databaseDB.ref('fcm-token/token').once('value')
-                .then(function (snapshot_in){
-                    const token = snapshot_in.val();
-                    console.log(token);
-                    sendMessage(token, 'Warning', "Có cái đầu vào nhà bạn")
-                    databaseDB.ref("notification/status").set("no");
-                })
+                    .then(function (snapshot_in) {
+                        const token = snapshot_in.val();
+                        console.log(token);
+                        sendMessage(token, 'Warning', "Có cái đầu buồi vào nhà bạn");
+                        databaseDB.ref("notification/status").set("no");
+                    })
             } else {
                 console.log("no")
             }
         })
 }, 1000);
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     databaseDB.ref("notification/status").set("yes");
     res.send("SP")
     console.log("Yes")
 })
 
+const POST = process.env.PORT || 3000;
 
-app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
+
+app.listen(POST, () => console.log('App is listening on url http://localhost:' + POST));
